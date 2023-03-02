57731601121f9623a00ad48f6ebb8c897105cbf0
nazarhussainh48 <nazar.hussain@atomicity-interactive.com>
2023-03-02 11:30:42 +0500

    Database Connection Success

---
+++ Week1Assignment1/Week1Assignment1/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Week1Assignment1.Models;
+
+namespace Week1Assignment1.Data
+{
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Data Context
+        /// </summary>
+        /// <param name="options"></param>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- Week1Assignment1/Week1Assignment1/Program.cs
+++ Week1Assignment1/Week1Assignment1/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Week1Assignment1.Data;
 using Week1Assignment1.Services.EmployeeService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddAutoMapper(typeof(Program));
 
